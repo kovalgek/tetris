1f4be88eefd835d28f91309eb07963036ffcbdcf
Anton Kovalchuk <antonk@entechsolutions.com>
2017-05-30 19:52:38 +0200

    first commit

---
+++ Tetris/Tetris/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tetris
+//
+//  Created by Anton Kovalchuk on 30.05.17.
+//  Copyright © 2017 Anton Kovalchuk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
